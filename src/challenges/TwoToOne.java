f035e5704944cb1e022a59782a668303a4d2418e
Carlos <carlosalejandres@gmail.com>
2019-09-02 20:06:33 +0200

    Funciona

--- src/challenges/TwoToOne.java
+++ src/challenges/TwoToOne.java
@@ -7,63 +7,82 @@ import java.util.List;
 import java.util.Comparator;
 
 public class TwoToOne {
-	
+
 	public static void main(String[] args) {
-		String s1 = "Hey there";
-		String s2 = "Hey you";
+		String s1 = "xyaabbbccccdefww";
+		String s2 = "xxxxyyyyabklmopq";
 		String resultado = longest2(s1, s2);
-		System.out.println("Este es el resultado " + resultado);
+		String longest = "abcdefklmopqwxy";
+		// System.out.println(longest == resultado);
+		// System.out.println("Este es el resultado " + resultado);
+		assertEquals("aehrsty", TwoToOne.longest2("aretheyhere", "yestheyarehere"));
 	}
-    
-    public static String longest1 (String s1, String s2) {
-        // your code
-    	StringBuilder sb = new StringBuilder();
-    	boolean noEsta = false;
-    	for (int i = 0; i < s1.length(); i++) {
-			for (int j = 1; j < sb.capacity(); j++) {
-				//System.out.println(sb.charAt(j));
-				if(s1.charAt(i) == sb.charAt(j)) {
-					noEsta = false;
-					break;
-				}
-				noEsta = true;
-			}
-			if (noEsta == true)
-				sb.append(s1.charAt(i));
+
+	private static boolean assertEquals(String string, String longest2) {
+		System.out.println("Hola");
+		if (string == longest2) {
+			System.out.println("hi");
+			return true;
+		} else {
+			System.out.println("wow");
+			return false;
 		}
-    	String stringResult = sb.toString();
-    	return stringResult;
-    }
-    
-    public static String longest2 (String s1, String s2) {
-    	StringBuilder sb = new StringBuilder();
-    	int contador=1;
-    	for (int i = 0; i < s1.length(); i++) {
+	}
+
+	// public static String longest1(String s1, String s2) {
+	// // your code
+	// StringBuilder sb = new StringBuilder();
+	// boolean noEsta = false;
+	// for (int i = 0; i < s1.length(); i++) {
+	// for (int j = 1; j < sb.capacity(); j++) {
+	// // System.out.println(sb.charAt(j));
+	// if (s1.charAt(i) == sb.charAt(j)) {
+	// noEsta = false;
+	// break;
+	// }
+	// noEsta = true;
+	// }
+	// if (noEsta == true)
+	// sb.append(s1.charAt(i));
+	// }
+	// String stringResult = sb.toString();
+	// return stringResult;
+	// }
+
+	public static String longest2(String s1, String s2) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < s1.length(); i++) {
 			sb.append(s1.charAt(i));
 		}
-    	for (int i = 0; i < s2.length(); i++) {
+		for (int i = 0; i < s2.length(); i++) {
 			sb.append(s2.charAt(i));
 		}
-		/*
-		 * System.out.println(sb.capacity()); System.out.println(sb.charAt(5));
-		 * sb.charAt(15); sb.deleteCharAt(5); sb.charAt(15);
-		 * System.out.println(sb.charAt(5)); System.out.println(sb.capacity());
-		 * System.out.println(sb.toString());
-		 */
-    	for (int i = 0; i < sb.capacity()-1; i++) {
-			for (int j = i+1; j < sb.capacity()-contador; j++) {
-				if(sb.charAt(i) == sb.charAt(j)) {
-					sb.deleteCharAt(j);
-					contador++;
-			}	
-				continue;
+
+		// System.out.println(sb.length());
+		// System.out.println(sb.charAt(0));
+		// sb.deleteCharAt(0);
+		// System.out.println(sb.charAt(sb.length()-1));
+		// System.out.println(sb.length());
+		// System.out.println(sb.length());
+
+		for (int i = 0; i < sb.length(); i++) {
+			for (int h = sb.length() - 1; h > 0; h--) {
+				// System.out.println("En el mas interno i es: " + i + " y j es: " + h);
+				if (i != h && sb.charAt(i) == sb.charAt(h)) {
+					// System.out.println("Antes de borrar: " + sb.toString());
+					sb.deleteCharAt(h);
+					// System.out.println("despues de borrar: " + sb.toString());
+				}
 			}
 		}
-    	String st = sb.toString();
-    	char[] array = st.toCharArray();
-    	Arrays.sort(array);
-    	System.out.println(array);
-    	st = Arrays.toString(array);
-    	return st;
-    }
+		// System.out.println(sb.toString());
+		String st = sb.toString();
+		char[] array = st.toCharArray();
+		Arrays.sort(array);
+		System.out.println(array);
+		st = Arrays.toString(array);
+		// System.out.println(st);
+		return new String(array);
+	}
+
 }
